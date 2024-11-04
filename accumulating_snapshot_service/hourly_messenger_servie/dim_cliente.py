297f0146826bc618b98a05c873cf00e46c1d38dd
DianaDev <micolta.diana@correounivalle.edu.co>
2024-11-04 15:35:03 -0500

    correccion dimension cliente, faltan las demas dimensiones

--- accumulating_snapshot_service/hourly_messenger_servie/dim_cliente.py
+++ accumulating_snapshot_service/hourly_messenger_servie/dim_cliente.py
@@ -1,25 +1,31 @@
-import pandas as pd
+import datetime
+from datetime import  date
 import numpy as np
-from datetime import datetime, timedelta
+import pandas as pd
+import yaml
+from sqlalchemy import create_engine
 
-def create_cliente_dim():
-    n_clients = 100
-    start_dates = [datetime(2023, 1, 1) - timedelta(days=np.random.randint(0, 365)) for _ in range(n_clients)]
-    
-    return pd.DataFrame({
-        'nit_cliente': range(1, n_clients + 1),
-        'nombre': [f'Cliente {i}' for i in range(1, n_clients + 1)],
-        'email': [f'cliente{i}@email.com' for i in range(1, n_clients + 1)],
-        'direccion': [f'Dirección {i}' for i in range(1, n_clients + 1)],
-        'telefono': [f'555-{str(i).zfill(4)}' for i in range(1, n_clients + 1)],
-        'nombre_contacto': [f'Contacto {i}' for i in range(1, n_clients + 1)],
-        'ciudad_id': np.random.randint(1, 21, n_clients),
-        'tipo_cliente_id': np.random.randint(1, 4, n_clients),
-        'activo': np.random.choice([True, False], n_clients),
-        'coordinador_id': np.random.randint(1, 11, n_clients),
-        'sector': np.random.choice(['Norte', 'Sur', 'Este', 'Oeste', 'Centro'], n_clients),
-        'tipo_cliente': np.random.choice(['corporativo', 'individual'], n_clients),
-        'clasificacion_cliente': np.random.choice(['A', 'B', 'C'], n_clients),
-        'fecha_primera_orden': start_dates,
-        'promedio_mensual_envios': np.random.randint(1, 100, n_clients)
-    })
+with open('../config.yml', 'r') as f:
+    config = yaml.safe_load(f)   
+    config_rf = config['fuente'] 
+    config_etl = config['bodega']
+
+# Construct the database URL
+url_rf = (f"{config_rf['drivername']}://{config_rf['user']}:{config_rf['password']}@{config_rf['host']}:"
+          f"{config_rf['port']}/{config_rf['dbname']}")
+url_etl = (f"{config_etl['drivername']}://{config_etl['user']}:{config_etl['password']}@{config_etl['host']}:"
+           f"{config_etl['port']}/{config_etl['dbname']}")
+# Create the SQLAlchemy Engine
+ra_fu = create_engine(url_rf)
+etl_conn = create_engine(url_etl)
+
+#loading table
+dim_cliente = pd.read_sql_table("cliente",etl_conn)
+
+
+dim_cliente["saved"] = date.today()
+dim_cliente.reset_index(drop=True, inplace=True)
+dim_cliente.head()
+dim_cliente.info()
+
+dim_cliente.tosql('dim_cliente', con=etl_conn, index_label='key_dim_cliente',if_exists='replace')
