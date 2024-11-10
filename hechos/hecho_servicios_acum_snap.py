18906ea53787ad01b003fa31064f09f49af092bf
daniel98 <daniel.andres.caicedo@correounivalle.edu.co>
2024-11-09 20:06:31 -0500

    hecho mensajería hora terminado

--- hechos/hecho_servicios_acum_snap.py
+++ hechos/hecho_servicios_acum_snap.py
@@ -8,7 +8,6 @@ def extract(source_db):
     # Primero, fusionamos df_agrupado con dim_fecha para obtener las claves de fecha
     dim_servicio = pd.read_sql_table('trans_servicio', source_db)
     dim_fecha = pd.read_sql_table('dim_fecha', source_db)
-    # dim_hora = pd.read_sql_table('dim_hora', source_db)
 
     # Merge para cada fecha que necesites
     dim_servicio = pd.merge(dim_servicio, dim_fecha[['date', 'key_dim_fecha']], left_on='fecha_iniciado', right_on='date', how='left')
@@ -31,28 +30,6 @@ def extract(source_db):
     dim_servicio.drop(columns=['date'], inplace=True)
     dim_servicio.rename(columns={'key_dim_fecha': 'key_fecha_terminado'}, inplace=True)
 
-
-    # LLAVES PRIMARIAS A DIMENSION HORA
-
-    # dim_servicio = pd.merge(dim_servicio, dim_hora[['time', 'key_dim_hora']], left_on='hora_iniciado', right_on='time', how='left')
-    # dim_servicio.drop(columns=['time'], inplace=True)
-    # dim_servicio.rename(columns={'key_dim_hora': 'key_hora_iniciado'}, inplace=True)
-
-    # dim_servicio = pd.merge(dim_servicio, dim_hora[['time', 'key_dim_hora']], left_on='hora_asignado', right_on='time', how='left')
-    # dim_servicio.drop(columns=['time'], inplace=True)
-    # dim_servicio.rename(columns={'key_dim_hora': 'key_hora_asignado'}, inplace=True)
-
-    # dim_servicio = pd.merge(dim_servicio, dim_hora[['time', 'key_dim_hora']], left_on='hora_recogido', right_on='time', how='left')
-    # dim_servicio.drop(columns=['time'], inplace=True)
-    # dim_servicio.rename(columns={'key_dim_hora': 'key_hora_recogido'}, inplace=True)
-
-    # dim_servicio = pd.merge(dim_servicio, dim_hora[['time', 'key_dim_hora']], left_on='hora_entregado', right_on='time', how='left')
-    # dim_servicio.drop(columns=['time'], inplace=True)
-    # dim_servicio.rename(columns={'key_dim_hora': 'key_hora_entregado'}, inplace=True)
-
-    # dim_servicio = pd.merge(dim_servicio, dim_hora[['time', 'key_dim_hora']], left_on='hora_terminado', right_on='time', how='left')
-    # dim_servicio.drop(columns=['time'], inplace=True)
-    # dim_servicio.rename(columns={'key_dim_hora': 'key_hora_terminado'}, inplace=True)
     return dim_servicio
   
   except Exception as e:
